58c128c971c31008a0e0ec54293521c98a5102b5
Ryan Kinney <ryan.kinney@maine.edu>
2022-01-26 16:37:58 -0500

    Fix question 4 up a bit.

--- hw01/hw1q4.c
+++ hw01/hw1q4.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /*
- * Prints all passed command line arguments to stdout.
+ * Prints all passed command line arguments to stdout,
+ * including argv[0], which contains the program name.
  * Each argument is printed on separate lines.
  */
 int main(int argc, char *argv[])
 {
-	for (int i = 1; i < argc; ++i) /* argv[0] contains the program name */
-		printf("%s\n",argv[i]);
+	for (int i = 0; i < argc; ++i) /* argc contains the number of args */
+		printf("Argument %i is %s\n", i, argv[i]);
 	return 0;
 }
 
