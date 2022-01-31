913d703b72e08a6f0acbc28b2f6a4d7f7eebf69c
Ryan Kinney <ryan.kinney@maine.edu>
2022-01-31 16:25:45 -0500

    Finish the last of the hw2 questions

---
+++ hw02/hw2q8.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/stat.h>
+
+/*
+ * Prints the total number of bytes used by a list of files passed on
+ * the command line. If stat fails to get information on a file,
+ * an error message is printed, and the file is skipped.
+ *
+ * If no names are passed on the command line, the program prints 0.
+ */
+int main(int argc, char *argv[])
+{
+	struct stat file; /* Holds the information from stat() */
+	uint64_t size = 0; /* Total size of the files in bytes */
+	int i;
+
+	/* argv[0] contains the name of the program; ignore it */
+	for (i = 1; i < argc; ++i) {
+		if (stat(argv[i], &file) == -1) { /* Attempt to get stats */
+			fprintf(stderr, "Error reading '%s': %s\n",
+				argv[i], strerror(errno));
+		}
+
+		size += (uint64_t) file.st_size;
+	}
+
+	/* Print size to stdout */
+	printf("%lld\n", size);
+	return 0;
+}
