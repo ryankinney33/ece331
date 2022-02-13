6a1517ef8dca942c4d06ba9241a2a7758fcc31f3
Ryan Kinney <ryan.kinney@maine.edu>
2022-02-13 12:03:45 -0500

    Answer a few questions from hw4

---
+++ hw04/hw4q6.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+import sys
+import requests
+
+# Extract the requested URL from the command line arguments
+try:
+    url_string = sys.argv[1]
+except IndexError:
+    sys.exit('Error: No URL specified.')
+
+# Extract the requested file from the URL
+pieces = url_string.split('/')
+fname = pieces[-1].split('?')[0] # also remove queries from the file name
+
+# There was no protocol, like http:// entered, or file name listed.
+# e.g, the requested URL was of the form 'google.com'
+# Insert http as the protocol and assume the requested file was index.html
+if len(pieces) == 1:
+    url_string = 'http://' + url_string
+    fname = 'index.html'
+
+# Check if there was no requested file name (URL ends in '/')
+elif not fname:
+    fname = 'index.html'
+
+print('Fetching:', url_string)
+while True:
+    # Try to read the URL and write the received content
+    r = requests.get(url_string)
+    try:
+        r.raise_for_status() # Raise an exception for an HTTP Error
+
+        with open(fname, 'wb') as f:
+            f.write(r.content)
+        break
+
+    # If the requested URL is a directory, add a '/' to the end of the URL
+    except IsADirectoryError:
+        url_string = url_string + '/'
+        fname = 'index.html'
+    except Exception as err:
+        sys.exit(str(err))
+
