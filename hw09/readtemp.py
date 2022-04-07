ef5577fcddf5595782e9a3bd0d77594c06a4a29f
Ryan Kinney <ryan.kinney@maine.edu>
2022-04-07 19:41:17 -0400

    Write the script for q2

--- hw09/readtemp.py
+++ hw09/readtemp.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python3
 import smbus
+import sqlite3
+from datetime import datetime
+
+# Get the current date and time
+now = datetime.now();
 
 # i2c information
 channel = 1
 i2c_addr = 0x48
 bus = smbus.SMBus(channel)
 
-# Read the data from the sensor
+# Read the data from the TC74 sensor
 TC74_temp = bus.read_byte(i2c_addr)
-print(TC74_temp)
 
 # Read the core temperature from sysfs
 with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
     core_temp = float(f.read())/1000 # temperature is read in millicelsius
 
-print(core_temp)
+# Open the database connection and set the cursor
+db = sqlite3.connect('temperatures.db')
+cursor = db.cursor()
 
+# Format the data into the sqlite command and insert the row
+cmd = 'INSERT INTO temps(DATETIME, TC74, CORE) VALUES ("{}", {}, {})'.format(
+        now, TC74_temp, core_temp)
+cursor.execute(cmd)
 
-# Add the data to the SQL database
+# Finally, commit the changes and exit
+db.commit()
+db.close()
